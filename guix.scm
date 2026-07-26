f6f1cb7fb2ccda79c993851c0c8585f81070dc0a
Jonathan D.A. Jewell <6759885+hyperpolymath@users.noreply.github.com>
2026-07-26 16:04:25 +0100

    chore: update guix.scm from squisher-corpus

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
 ; SPDX-License-Identifier: MPL-2.0
-;; guix.scm — GNU Guix package definition for universal-language-server-plugin
+;; guix.scm — GNU Guix package definition for squisher-corpus
 ;; Usage: guix shell -f guix.scm
 
 (use-modules (guix packages)
@@ -7,12 +7,12 @@
              (guix licenses))
 
 (package
-  (name "universal-language-server-plugin")
+  (name "squisher-corpus")
   (version "0.1.0")
   (source #f)
   (build-system gnu-build-system)
-  (synopsis "universal-language-server-plugin")
-  (description "universal-language-server-plugin — part of the hyperpolymath ecosystem.")
-  (home-page "https://github.com/hyperpolymath/universal-language-server-plugin")
-  (license ((@@ (guix licenses) license) "MPL-2.0"
+  (synopsis "squisher-corpus")
+  (description "squisher-corpus — part of the hyperpolymath ecosystem.")
+  (home-page "https://github.com/hyperpolymath/squisher-corpus")
+  (license ((@@ (guix licenses) license) "PMPL-1.0-or-later"
              "https://github.com/hyperpolymath/palimpsest-license")))
